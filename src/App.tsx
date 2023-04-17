b934de2372666eadd9166042620cb68a32a5c4ee
SandraYushko <shyria6@gmail.com>
2023-04-17 23:56:14 +0300

    Lesson 09 (import/export)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import Technologies from "./Technologies"
 
 function App() {
     return (
@@ -7,29 +9,6 @@ function App() {
             <div> Hello, samurai! Let's go!</div>
             <div> <Header /></div>
             <div> <Technologies /></div>
-        </div>
-    );
-}
-
-function Header() {
-    return (
-        <div>
-            <a href='#s'>Home</a><br/>
-            <a href='#s'>News Feed</a><br/>
-            <a href='#s'>Messages</a><br/>
-        </div>
-    );
-}
-
-function Technologies() {
-    return (
-        <div>
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
         </div>
     );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <a href='#s'>Home</a><br/>
+            <a href='#s'>News Feed</a><br/>
+            <a href='#s'>Messages</a><br/>
+        </div>
+    );
+}
+
+export default Header;
