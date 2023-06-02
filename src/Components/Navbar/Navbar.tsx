ce05c9c46939ab4d864062ef3ad1bd0d4a11c76a
SandraYushko <shyria6@gmail.com>
2023-06-03 02:10:30 +0300

    Lesson 20 (NavLink)

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from './Navbar.module.css'
+import {NavLink} from 'react-router-dom';
 
 export const Navbar = () => {
     return <nav className={classes.nav}>
-        <div className={classes.item}><a href="/profile">Profile</a></div>
-        <div className={`${classes.item} ${classes.active}`}><a href="/dialogs">Messages</a></div>
-        <div className={classes.item}><a href="/news">News</a></div>
-        <div className={classes.item}><a href="/music">Music</a></div>
-        <div className={classes.item}><a href="/settings">Settings</a></div>
+        <div className={classes.item}><NavLink to="/profile" className={(navData) =>(navData ? classes.active : classes.item)}>Profile</NavLink></div>
+        <div className={`${classes.item} ${classes.active}`}><NavLink to="/dialogs" className={(navData) =>(navData ? classes.active : classes.item)}>Messages</NavLink></div>
+        <div className={classes.item}><NavLink to="/news" className={(navData) =>(navData ? classes.active : classes.item)}>News</NavLink></div>
+        <div className={classes.item}><NavLink to="/music" className={(navData) =>(navData ? classes.active : classes.item)}>Music</NavLink></div>
+        <div className={classes.item}><NavLink to="/settings" className={(navData) =>(navData ? classes.active : classes.item)}>Settings</NavLink></div>
     </nav>
 }
