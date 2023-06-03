24080ea70164079f801147e16ba00f96fdff4d6d
SandraYushko <shyria6@gmail.com>
2023-06-03 22:02:05 +0300

    Lesson 21 (verstaem stranitcy dialogov)

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -3,8 +3,19 @@ import classes from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>Sasha</div>
+                <div className={classes.dialog}>Masha</div>
+                <div className={classes.dialog}>Dasha</div>
+                <div className={classes.dialog}>Kate</div>
+                <div className={classes.dialog}>Sveta</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}> Hello </div>
+                <div className={classes.message}> How are you? </div>
+                <div className={classes.message}> Yo </div>
+            </div>
         </div>
     )
 }
