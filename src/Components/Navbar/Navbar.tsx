7cd51ee42c1d8d8cf621e279f2f54f180b23edd3
SandraYushko <shyria6@gmail.com>
2023-06-03 19:52:52 +0300

    Lesson 20 (NavLink + ispravlen CSS)

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,11 +4,11 @@ import {NavLink} from 'react-router-dom';
 
 export const Navbar = () => {
     return <nav className={classes.nav}>
-        <div className={classes.item}><NavLink to="/profile" className={(navData) =>(navData ? classes.active : classes.item)}>Profile</NavLink></div>
-        <div className={`${classes.item} ${classes.active}`}><NavLink to="/dialogs" className={(navData) =>(navData ? classes.active : classes.item)}>Messages</NavLink></div>
-        <div className={classes.item}><NavLink to="/news" className={(navData) =>(navData ? classes.active : classes.item)}>News</NavLink></div>
-        <div className={classes.item}><NavLink to="/music" className={(navData) =>(navData ? classes.active : classes.item)}>Music</NavLink></div>
-        <div className={classes.item}><NavLink to="/settings" className={(navData) =>(navData ? classes.active : classes.item)}>Settings</NavLink></div>
+        <div className={classes.item}><NavLink to="/profile" >Profile</NavLink></div>
+        <div className={classes.item}><NavLink to="/dialogs" >Messages</NavLink></div>
+        <div className={classes.item}><NavLink to="/news" >News</NavLink></div>
+        <div className={classes.item}><NavLink to="/music" >Music</NavLink></div>
+        <div className={classes.item}><NavLink to="/settings" >Settings</NavLink></div>
 
     </nav>
 }
