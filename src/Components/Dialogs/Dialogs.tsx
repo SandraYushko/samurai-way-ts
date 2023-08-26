47ca9fa8f1820b5a30ef08f99ed1d195d0ad5179
SandraYushko <shyria6@gmail.com>
2023-08-26 14:02:43 +0300

    Lesson 24 - UI - BLL, выносим данные в сторону

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -23,20 +23,49 @@ const Message = (props: MessagePropsType) => {
 
 
 export const Dialogs = () => {
+    let dialogsData = [
+        {id:1, name:"Sasha"},
+        {id:2, name:"Masha"},
+        {id:3, name:"Dasha"},
+        {id:4, name:"Kate"},
+        {id:5, name:"Sveta"}
+    ]
+    let messagesData = [
+        {id:1, message:"Hello"},
+        {id:2, message:"How are you?"},
+        {id:3, message:"Yo"},
+    ]
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name="Sasha" id={1}/>
-                <DialogItem name="Masha" id={2}/>
-                <DialogItem name="Dasha" id={3}/>
-                <DialogItem name="Kate" id={4}/>
-                <DialogItem name="Sveta" id={5}/>
+                {dialogsData.map((el) => {return <DialogItem name={el.name} id={el.id}/>})}
             </div>
             <div className={classes.messages}>
-                <Message message="Hello"/>
-                <Message message="How are you?"/>
-                <Message message="Yo"/>
+                {messagesData.map((el) => {return  <Message message={el.message}/>})}
             </div>
         </div>
     )
 }
+
+
+
+
+
+// return (
+//     <div className={classes.dialogs}>
+//         <div className={classes.dialogsItems}>
+//             <DialogItem name="Sasha" id={1}/>
+//             <DialogItem name="Masha" id={2}/>
+//             <DialogItem name="Dasha" id={3}/>
+//             <DialogItem name="Kate" id={4}/>
+//             <DialogItem name="Sveta" id={5}/>
+//         </div>
+//         <div className={classes.messages}>
+//             <Message message="Hello"/>
+//             <Message message="How are you?"/>
+//             <Message message="Yo"/>
+//         </div>
+//     </div>
+// )
+// }
--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -4,6 +4,12 @@ import {Post} from "./Posts/Post";
 
 
 export const MyPosts = () => {
+    let postsData = [
+        {id:1, message:"Hi, how are you?", likesCount:5},
+        {id:2, message:"It's my first post", likesCount:3},
+        {id:3, message:"+++", likesCount:50}
+    ]
+
     return <div className={classes.postsBlock}>
         <div> <h3>My posts</h3>
             <div>
@@ -20,9 +26,31 @@ export const MyPosts = () => {
             </div>
         </div>
         <div className={classes.posts}>
-            <Post message='Hi, how are you?' likesCount={5}/>
-            <Post message="It's my first post" likesCount={3}/>
-            <Post message='3' likesCount={50}/>
+            {postsData.map((el) => {return <Post message={el.message} likesCount={el.likesCount}/>})}
         </div>
     </div>
 }
+
+
+// return <div className={classes.postsBlock}>
+//     <div> <h3>My posts</h3>
+//         <div>
+//             <div>
+//                 <textarea></textarea>
+//             </div>
+//             <div>
+//                 <button>Add post</button>
+//                 <button>Remove</button>
+//             </div>
+//         </div>
+//         <div>
+//             <h4>New post</h4>
+//         </div>
+//     </div>
+//     <div className={classes.posts}>
+//         <Post message='Hi, how are you?' likesCount={5}/>
+//         <Post message="It's my first post" likesCount={3}/>
+//         <Post message='3' likesCount={50}/>
+//     </div>
+// </div>
+// }
